b915d4d00730920bf4bf4ad6f4f041a26f2ca20c
Bruno Sandande <brunosanda63@gmail.com>
2021-11-19 09:00:54 +0100

    atualizacao

--- lib/griddashboard.dart
+++ lib/griddashboard.dart
@@ -1,27 +1,111 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class GridDashboard extends StatelessWidget {
-  Item item1 = new Item(
-    titulo: "Calendar",
-    subTitulo: "Março, Quarta feira",
-    evento: "3 Eventos",
-    imagem: "assets/calendar.png",
+  Items item1 = new Items(
+      title: "Calendar",
+      subtitle: "March, Wednesday",
+      event: "3 Events",
+      img: "assets/images/calendar.png");
+
+  Items item2 = new Items(
+    title: "Groceries",
+    subtitle: "Bocali, Apple",
+    event: "4 Items",
+    img: "assets/images/calendar.png",
+  );
+  Items item3 = new Items(
+    title: "Locations",
+    subtitle: "Lucy Mao going to Office",
+    event: "",
+    img: "assets/images/calendar.png",
+  );
+  Items item4 = new Items(
+    title: "Activity",
+    subtitle: "Rose favirited your Post",
+    event: "",
+    img: "assets/images/calendar.png",
+  );
+  Items item5 = new Items(
+    title: "To do",
+    subtitle: "Homework, Design",
+    event: "4 Items",
+    img: "assets/images/calendar.png",
+  );
+  Items item6 = new Items(
+    title: "Settings",
+    subtitle: "",
+    event: "2 Items",
+    img: "assets/images/calendar.png",
   );
 
   @override
   Widget build(BuildContext context) {
-    return new Card();
+    List<Items> myList = [item1, item2, item3, item4, item5, item6];
+    var color = 0xff453658;
+    return Flexible(
+      child: GridView.count(
+          childAspectRatio: 1.0,
+          padding: EdgeInsets.only(left: 16, right: 16),
+          crossAxisCount: 2,
+          crossAxisSpacing: 18,
+          mainAxisSpacing: 18,
+          children: myList.map((data) {
+            return Container(
+              decoration: BoxDecoration(
+                  color: Color(color), borderRadius: BorderRadius.circular(10)),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Image.asset(
+                    data.img,
+                    width: 42,
+                  ),
+                  SizedBox(
+                    height: 14,
+                  ),
+                  Text(
+                    data.title,
+                    style: GoogleFonts.openSans(
+                        textStyle: TextStyle(
+                            color: Colors.white,
+                            fontSize: 16,
+                            fontWeight: FontWeight.w600)),
+                  ),
+                  SizedBox(
+                    height: 8,
+                  ),
+                  Text(
+                    data.subtitle,
+                    style: GoogleFonts.openSans(
+                        textStyle: TextStyle(
+                            color: Colors.white38,
+                            fontSize: 10,
+                            fontWeight: FontWeight.w600)),
+                  ),
+                  SizedBox(
+                    height: 14,
+                  ),
+                  Text(
+                    data.event,
+                    style: GoogleFonts.openSans(
+                        textStyle: TextStyle(
+                            color: Colors.white70,
+                            fontSize: 11,
+                            fontWeight: FontWeight.w600)),
+                  ),
+                ],
+              ),
+            );
+          }).toList()),
+    );
   }
 }
 
-class Item {
-  String titulo;
-  String subTitulo;
-  String evento;
-  String imagem;
-  Item(
-      {required this.titulo,
-      required this.subTitulo,
-      required this.evento,
-      required this.imagem});
+class Items {
+  String title;
+  String subtitle;
+  String event;
+  String img;
+  Items({required this.title, required this.subtitle, required this.event, required this.img});
 }
